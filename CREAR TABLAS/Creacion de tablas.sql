ac314b5b6940e0933e3343da7952ba2d1b46fa46
rodrigo sebastian vega centeno <75128404+rodrigo1857@users.noreply.github.com>
2025-07-16 14:14:34 -0500

    Update Creacion de tablas.sql

--- CREAR TABLAS/Creacion de tablas.sql
+++ CREAR TABLAS/Creacion de tablas.sql
@@ -8,14 +8,13 @@ SET search_path TO sistema_informacion_gerencial;
 
 create table if not exists sistema_informacion_gerencial.dm_area
 (
-    cod_siaf_area     varchar not null
-        constraint dm_area_pk
-            primary key,
-    area_name         char(6) not null,
-    area_display_name varchar not null,
-    nivel             integer,
-    id_area           integer,
-    idsuperior        integer
+    area_siaf   varchar not null
+        primary key,
+    cod_area    char(6) not null,
+    desc_area   varchar not null,
+    nivel       integer,
+    id_area     integer,
+    id_superior integer
 );
 
 alter table sistema_informacion_gerencial.dm_area
@@ -24,132 +23,153 @@ alter table sistema_informacion_gerencial.dm_area
 create table if not exists sistema_informacion_gerencial.dm_fuente
 (
     fuente_siaf varchar not null
-        constraint dm_fuente_pk
-            primary key,
+        primary key,
     desc_fuente varchar not null
 );
 
 alter table sistema_informacion_gerencial.dm_fuente
     owner to postgres;
 
-
-
 create table if not exists sistema_informacion_gerencial.dm_generica
 (
     id_generica   integer not null
-        constraint dm_generica_pk
-            primary key,
-    cod_generica  varchar
-        constraint dm_generica_pk_2
+        primary key,
+    generica_siaf varchar
+        constraint dm_generica_cod_generica_key
             unique,
-    nomb_generica varchar
+    desc_generica varchar
 );
 
 alter table sistema_informacion_gerencial.dm_generica
     owner to postgres;
 
-
-
-create table if not exists sistema_informacion_gerencial.hechos_pim
-(
-    id_area        integer,
-    cod_siaf_area  varchar,
-    id_fuente      integer        not null,
-    monto_pia      numeric(19, 2) not null,
-    monto_pim      numeric(19, 2) not null,
-    id_periodo_pla integer        not null,
-    generica       varchar,
-    id_generica    integer
-        constraint hechos_pim_dm_generica_id_generica_fk
-            references sistema_informacion_gerencial.dm_generica,
-    fuente_siaf    varchar
-);
-
-alter table sistema_informacion_gerencial.hechos_pim
-    owner to postgres;
-
-
-
 create table if not exists sistema_informacion_gerencial.hechos_institucional_consolidados
 (
-    cod_siaf_area   varchar        not null
-        constraint hechos_institucional_consolidados_dm_area_cod_siaf_area_fk
-            references sistema_informacion_gerencial.dm_area,
+    area_siaf       varchar        not null,
     num_certificado varchar        not null,
     anio            integer        not null,
     monto           numeric(19, 2) not null,
     constraint hechos_institucional_consolidados_pk
-        unique (num_certificado, anio)
-);
+        primary key (anio, num_certificado)
+)
+    partition by LIST (anio);
 
 alter table sistema_informacion_gerencial.hechos_institucional_consolidados
     owner to postgres;
 
-create table if not exists sistema_informacion_gerencial.dm_certificado
-(
-    ano_eje              integer,
-    num_certificado      varchar,
-    cod_siaf_area        varchar,
-    secuencia            varchar,
-    sec_ejec             varchar,
-    monto_clasificador   numeric(19, 2),
-    siaf_id_fuente       varchar
-        constraint dm_certificado_dm_fuente_fuente_siaf_fk
-            references sistema_informacion_gerencial.dm_fuente,
-    glosa                varchar,
-    correlativo          varchar,
-    siaf_id_clasificador varchar,
-    clasificador         varchar,
-    generica             varchar
-        constraint dm_certificado_dm_generica_cod_generica_fk
-            references sistema_informacion_gerencial.dm_generica (cod_generica),
-    cod_doc              varchar,
-    num_doc              varchar,
-    estado_envio         varchar,
-    estado_registro      varchar,
-    fecha_creacion_clt   date,
-    idmeta               varchar,
-    codmeta              varchar,
-    nomb_met_ins         varchar,
-    constraint dm_certificado_hechos_institucional_consolidados_anio_num_certi
-        foreign key (ano_eje, num_certificado) references sistema_informacion_gerencial.hechos_institucional_consolidados ()
-);
-
-alter table sistema_informacion_gerencial.dm_certificado
-    owner to postgres;
-
 create table if not exists sistema_informacion_gerencial.dm_expediente
 (
-    ano_eje               integer not null,
-    sec_ejec              char(6),
-    area_siaf             varchar,
-    expediente            varchar,
+    anio                  integer not null,
+    ejecutora             char(6),
+    area_siaf             varchar
+        constraint dm_expediente_dm_area_area_siaf_fk
+            references sistema_informacion_gerencial.dm_area,
+    expediente            varchar not null,
     fase                  varchar,
-    secuencia             varchar,
-    certificado_secuencia varchar,
-    correlativo           varchar,
+    secuencia             varchar not null,
+    correlativo           varchar not null,
     ciclo                 varchar,
     certificado           varchar,
+    certificado_secuencia varchar,
     fecha_autorizacion    date,
     fuente_siaf           varchar
         constraint dm_expediente_dm_fuente_fuente_siaf_fk
             references sistema_informacion_gerencial.dm_fuente,
     clasificador          varchar,
-    generica              varchar
-        constraint dm_expediente_dm_generica_cod_generica_fk
-            references sistema_informacion_gerencial.dm_generica (cod_generica),
+    generica_siaf         varchar
+        constraint dm_expediente_dm_generica_generica_siaf_fk
+            references sistema_informacion_gerencial.dm_generica (generica_siaf),
     monto_nacional        numeric(19, 2),
-    cod_doc               varchar,
+    cod_doc               varchar not null,
     num_doc               varchar,
     estado_envio          varchar,
-    siaf_id_clasificador  varchar,
-    constraint dm_expediente_hechos_institucional_consolidados_anio_num_certif
-        foreign key (ano_eje, certificado) references sistema_informacion_gerencial.hechos_institucional_consolidados ()
-);
+    idclasificador_siaf   varchar not null,
+    trimestre             integer,
+    constraint dm_expediente_pk
+        primary key (anio, expediente, secuencia, correlativo, cod_doc, idclasificador_siaf)
+)
+    partition by LIST (anio);
 
 alter table sistema_informacion_gerencial.dm_expediente
     owner to postgres;
 
+create table if not exists sistema_informacion_gerencial.hechos_pim
+(
+    anio          integer not null,
+    ejecutora     varchar not null,
+    fuente_siaf   varchar not null,
+    generica_siaf varchar not null,
+    monto_pia     numeric(19, 2),
+    monto_pim     numeric(19, 2),
+    constraint hechos_pim_pk
+        primary key (anio, fuente_siaf, generica_siaf)
+);
+
+alter table sistema_informacion_gerencial.hechos_pim
+    owner to postgres;
+
+create table if not exists sistema_informacion_gerencial.dm_pim
+(
+    anio          integer        not null,
+    id_fuente     integer        not null,
+    fuente_siaf   varchar        not null
+        references sistema_informacion_gerencial.dm_fuente,
+    id_area       integer,
+    area_siaf     varchar,
+    id_generica   integer
+        references sistema_informacion_gerencial.dm_generica,
+    monto_pia     numeric(19, 2) not null,
+    monto_pim     numeric(19, 2) not null,
+    generica_siaf varchar,
+    constraint dm_pim_hechos_pim_anio_fuente_siaf_generica_siaf_fk
+        foreign key (anio, fuente_siaf, generica_siaf) references sistema_informacion_gerencial.hechos_pim
+);
+
+alter table sistema_informacion_gerencial.dm_pim
+    owner to postgres;
+
+
+
+create table if not exists sistema_informacion_gerencial.dm_certificado
+(
+    anio                integer not null,
+    num_certificado     varchar not null,
+    area_siaf           varchar
+        constraint dm_certificado_dm_area_area_siaf_fk
+            references sistema_informacion_gerencial.dm_area,
+    secuencia           varchar not null,
+    ejecutora           varchar,
+    monto_clasificador  numeric(19, 2),
+    fuente_siaf         varchar
+        constraint dm_certificado_dm_fuente_fuente_siaf_fk
+            references sistema_informacion_gerencial.dm_fuente,
+    glosa               varchar,
+    correlativo         varchar not null,
+    idclasificador_siaf varchar not null,
+    clasificador        varchar,
+    generica_siaf       varchar
+        constraint dm_certificado_dm_generica_generica_siaf_fk
+            references sistema_informacion_gerencial.dm_generica (generica_siaf),
+    cod_doc             varchar,
+    num_doc             varchar,
+    estado_envio        varchar,
+    estado_registro     varchar,
+    fecha_creacion_clt  date,
+    idmeta              varchar not null,
+    codmeta             varchar,
+    nomb_met_ins        varchar,
+    constraint dm_certificado_pk
+        primary key (anio, num_certificado, secuencia, correlativo, idmeta, idclasificador_siaf),
+    constraint dm_certificado_hechos_institucional_consolidados_num_certificad
+        foreign key (num_certificado, anio) references sistema_informacion_gerencial.hechos_institucional_consolidados (),
+    foreign key (num_certificado, anio) references sistema_informacion_gerencial.hechos_institucional_consolidados_2024 (),
+    constraint dm_certificado_num_certificado_anio_fkey1
+        foreign key (num_certificado, anio) references sistema_informacion_gerencial.hechos_institucional_consolidados_2025 ()
+)
+    partition by LIST (anio);
+
+alter table sistema_informacion_gerencial.dm_certificado
+    owner to postgres;
 
 
 -------8
